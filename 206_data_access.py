21c6d58fd7421d4e2a08feffbf087a7bb945605d
Michael Braunstein <mbbraun@umich.edu>
2017-04-24 13:58:49 -0400

    Making some changes

--- 206_data_access.py
+++ 206_data_access.py
@@ -21,7 +21,6 @@ import unittest
 import itertools
 import requests
 import collections 
-import re
 import tweepy
 import twitter_info
 import json
@@ -52,14 +51,12 @@ except:
 # Here I will create a function to pull data from OMDb API and caching it as well: 
 # This function will take input of a movie title and then return a dictionary of data about that movie, and cache the data and the cache file. 
 def get_movie_data(movie_str):
-	unique_identifier = "OMBb_{}".format(movie_str)
+	unique_identifier = "OMDb_{}".format(movie_str)
 	if unique_identifier in CACHE_DICTION:
-		print ("Using cached data for", movie_str)
-		print("\n")
+		#print ("Using cached data for", movie_str)
 		movie_dict = CACHE_DICTION[unique_identifier]
 	else:
-		print ("getting data from internet for", movie_str)
-		print ("\n")
+		#print ("getting data from internet for", movie_str)
 		baseurl = 'http://www.omdbapi.com/?'
 		url_params = {'t': movie_str, 'y': '2017'}
 		response = requests.get(baseurl, params = url_params)
@@ -76,12 +73,10 @@ def get_movie_data(movie_str):
 def get_twitter_data(search):
 	unique_identifier = "twitter_{}".format(search)
 	if unique_identifier in CACHE_DICTION:
-		print ("using cached data for", search)
-		print ("\n")
+		#print ("using cached data for", search)
 		twitter_results = CACHE_DICTION[unique_identifier]
 	else:
-		print ("getting data from internet for", search)
-		print ("\n")
+		#print ("getting data from internet for", search)
 		twitter_results = api.search(q=search)
 		CACHE_DICTION[unique_identifier] = twitter_results
 		f = open(CACHE_FNAME, 'w')
@@ -116,7 +111,7 @@ cur.execute(table_spec)
 #Tweets table
 table_spec = 'CREATE TABLE IF NOT EXISTS '
 table_spec += 'Tweets (tweet_id TEXT PRIMARY KEY, '
-table_spec += 'tweet_text TEXT, user_id TEXT, associated_movie TEXT, num_retweets INTEGER)'
+table_spec += 'tweet_text TEXT, user_id TEXT, associated_movie TEXT, num_retweets INTEGER, num_favorites INTEGER)'
 cur.execute(table_spec)
 
 #Users table
@@ -127,7 +122,7 @@ cur.execute(table_spec)
 
 # Here are my three insert statements I will call later in order to input my data into my database. 
 statement = 'INSERT OR IGNORE INTO Movies VALUES (?, ?, ?, ?, ?, ?, ?)'
-statement1 = 'INSERT OR IGNORE INTO Tweets VALUES (?, ?, ?, ?, ?)'
+statement1 = 'INSERT OR IGNORE INTO Tweets VALUES (?, ?, ?, ?, ?, ?)'
 statement2 = 'INSERT OR IGNORE INTO Users VALUES (?, ?, ?)'
 
 
@@ -158,7 +153,7 @@ class Movie(): #This is my creation of the class
 
 	
 	def __str__(self): # Here is my string method that returns a string about the movie. This will focus on certain aspects fo the movie that I think are important, such as the title, who it was directed by, notable actors, and rating.  
-		return self.title + " Was directed by " + self.director  +" with notable actors being " + self.get_actors() + ". This movie had an IMDM rating of " + self.rating
+		return self.title + " was directed by " + self.director  +" with notable actors being " + self.get_actors() + ". This movie had an IMDM rating of " + str(self.rating) + " and a box office performance of " + self.box_office + ". It was created in " + str(self.languages) + " language(s) and came out in " + str(self.year) + "."
 
 # This is where I create my list of movies I will find data on, stored in variable movies
 movies = ["Beauty and the Beast", "The Boss Baby", "Logan"]
@@ -188,14 +183,15 @@ class Twitter_search():
 		self.retweets = tweet['retweet_count']
 		self.associated_movie = movie
 		self.screen_name = tweet['user']['screen_name']
-		self.favorites = tweet['user']['favourites_count']
+		self.favorites = tweet['favorite_count']
+		self.user_favorites = tweet["user"]["favourites_count"]
 
 	# This method, get_twitter_table will return the tuple of information about each tweet that I can then load into the data table. I figured that I can reutrn the data and then inout it right into the table later on in the code. 
 	def get_twitter_table(self):
-		t1 = self.id, self.text, self.userid, self.associated_movie, self.retweets
+		t1 = self.id, self.text, self.userid, self.associated_movie, self.retweets, self.favorites
 		return t1
 	def get_users_table(self):
-		t2 = self.userid, self.screen_name, self.favorites
+		t2 = self.userid, self.screen_name, self.user_favorites
 		return t2
 
 # This is where I am defining my hashtags that I will collect data on, hashtags to be stored in variable hashtags
@@ -240,59 +236,130 @@ for tweets in hashtag_tweets:
 conn.commit()
 
 
+file_summary = open("final_project_summary.txt", 'w')
+file_summary.write("Michael Braunstein\n")
+file_summary.write("SI 206 Final Project Output Summary Page\n\n")
+file_summary.write("For this project, I performed a lot of data analytics on current movies and how much people were talking about each respective movie on Twitter. This summary page should give you a better idea of what movie you should go see based on these statistics. \n\nThe movies I decided to look at were Beauty and the Beast, The Boss Baby, and Logan. ")
+
 # Query statements:
 
 
-# Pull all of the data from the Tweets table for tweets that have over 50 retweets, and save that data in list with variable name being more_than_50_rts
-statement = 'SELECT * FROM Tweets WHERE num_retweets > 50'
+# Pull all of the data from the Tweets table where number of retweets is greater than 0, and save that data in list with variable name being tweets_for_rts
+statement = 'SELECT * FROM Tweets WHERE num_retweets > 20'
 result = cur.execute(statement)
-more_than_50_rts = []
+tweets_for_rts = []
 for r in result.fetchall():
-	more_than_50_rts.append(r)
+	tweets_for_rts.append(r)
+
+# Sort this list by the number of retweets and return a list of the 3 most retweeted tweets, save in in a variable named most_popular_tweets
+sorted_tweets_for_rts = sorted(tweets_for_rts, key = lambda x: x[4], reverse = True)
+most_popular_tweets = sorted_tweets_for_rts[:3]
+most_popular_tweets_str = "After searching for tweets about each movie, here were the three most popular tweets and a little information about each of them. Note: Popular tweets means they had the most retweets\n" 
+file_summary.write(most_popular_tweets_str)
+for i in range(len(most_popular_tweets)):
+	file_summary.write("\n")
+	file_summary.write(str(i+1) + ". Tweet Text: " +str(most_popular_tweets[i][1]))
+	file_summary.write( "\nAssociated Movie: " + str(most_popular_tweets[i][3]))
+	file_summary.write( "\nTweet Id: "+ str(most_popular_tweets[i][0]))
+	file_summary.write( "\nUser Id: "+ str(most_popular_tweets[i][2]))
+	file_summary.write( "\nNumber of Retweets: " + str(most_popular_tweets[i][4]))
+	file_summary.write("\n")
 
 
 
-# Make an inner join query statement to find the screen name, associated movie, user favorites and their number of retweets on their tweet from the inner join of Tweets and Users tables where the number of rewtweets and user favorites are both greater than 25, then save that resulting list in a variable most_popular_movies_tweeters
-statement = 'SELECT screen_name, associated_movie, user_favorites, num_retweets FROM Tweets INNER JOIN Users ON Tweets.user_id=Users.user_id WHERE num_retweets >25 AND user_favorites > 25'
+
+
+#print (tweets_for_rts)
+
+
+# Make an inner join query statement to find the screen name, associated movie, user favorites and their number of retweets on their tweet from the inner join of Tweets and Users tables where the number of rewtweets and user favorites are both greater than 50, then save that resulting list of tuples in a dictionary named variable most_popular_movies_tweeters
+# Make sure you do this using dictonary comprehension
+statement = 'SELECT screen_name, associated_movie, user_favorites, num_retweets FROM Tweets INNER JOIN Users ON Tweets.user_id=Users.user_id WHERE num_retweets >50 AND user_favorites > 50'
 result = cur.execute(statement)
-most_popular_movies_tweeters = []
-for r in result.fetchall():
-	most_popular_movies_tweeters.append(r)
+#most_popular_movies_tweeters = {r[1]:(r[0],r[2],r[3]) for r in result.fetchall()}
+most_popular_movies_tweeters = {r[0]:(r[1],r[2],r[3]) for r in result.fetchall()}
+# sorted_most_popular_movies_tweeters = sorted(most_popular_movies_tweeters, key = lambda x: most_popular_movies_tweeters[x][0])
+# print (sorted_most_popular_movies_tweeters)
+# for r in result.fetchall():
+# 	most_popular_movies_tweeters.append(r)
+print (most_popular_movies_tweeters)
+print ('**************')
 
+tweeters_str = "\nWhen looking at users who tweeted about certain movies, I looked at what popular users (people who had over 50 user favorites and 50 retweets on their tweet) and what movie they tweeted about. It becomes evident that certain movies attract more attention than others:\n"
+file_summary.write(tweeters_str)
+file_summary.write("The statistics after the username are as follows: (Movie tweeted about, number of user favorites, number of retweets)\n")
+for key in most_popular_movies_tweeters.keys():
+	file_summary.write("\n")
+	file_summary.write("User: " + key + "\n Associated Movie: " +str(most_popular_movies_tweeters[key][0]) + "\n User Favorites: " + str(most_popular_movies_tweeters[key][1]) + "\n Number of Tweet Retweets: " + str(most_popular_movies_tweeters[key][2]))
+	file_summary.write("\n")
 
-#Use dictionary accumulation in order to calculate the total number of retweets each of the associated movies have had. 
-# Each kep should be the associated movie and each value should add all of the retweets together. 
-
+# Use dictionary accumulation in order to calculate the total number of retweets each of the associated movies have had. 
+# Each key should be the associated movie and each value should add all of the retweets together. 
+movie_retweets = {}
+for tweet in tweets_for_rts:
+	if tweet[3] not in movie_retweets:
+		movie_retweets[tweet[3]] = tweet[4]
+	else:
+		movie_retweets[tweet[3]] += tweet[4]
+file_summary.write("\n\nWhen looking at each movie, here are the total number of retweets on the tweets pulled associated to each movie:\n")
+for movie in movie_retweets.keys():
+	file_summary.write(movie + " Retweets:  " + str(movie_retweets[movie]))
+	file_summary.write("\n")
+# print (movie_retweets)
 
 # Then sort a list of the dictionary keys based on the number of total number of retweets for each movie, and store the top movie and its number of retweets in a varaiable called top_movie_retweets.
+sorted_movie_retweets = sorted(movie_retweets, key = lambda x: movie_retweets[x], reverse = True)
+#print (sorted_movie_retweets)
+top_movie_retweets = [sorted_movie_retweets[0], movie_retweets[sorted_movie_retweets[0]]]
+file_summary.write("\nThe most tweeted about movie: " +str(top_movie_retweets[0]) + " with " +str(top_movie_retweets[1]) + " retweets!\n")
+print (top_movie_retweets)
 
 
-
-# Pull the movie title, rating, and box office performance for each movie with a query statment. Save this information in a list named movie_performances
+# Pull the movie title, rating, and box office performance for each movie with a query statment. Save this information in a list named movie_performances.
+# Save this information by using list comprehension
+statement = 'SELECT title, rating, box_office FROM Movies'
+result = cur.execute(statement)
+movie_performances = [r for r in result.fetchall()]
+sorted_movie_performances = sorted(movie_performances, key = lambda x: x[1], reverse = True)
+file_summary.write("\nHere were the performances of each of the three movies (listed by their ratings), found from the IMDb database:\n\n")
+for movie in sorted_movie_performances:
+	file_summary.write(movie[0] + ": Rating: " + str(movie[1]) + " Box Office Performance: " + str(movie[2]))
+	file_summary.write("\n")
+#print (movie_performances)
 
 
 # Use a query to pull the screen_name and user favorites from the Users table for users that have over 50 favorites and save the resulting tuples in a list named top_users 
 
 
 
-# Use set comprehension on the top_users to find the users that favorite the most tweets and sort to have only the top 5 users, in essence people that use Twitter a lot and save that in varaible top_5 users. 
-
-
 # Use dictionary comprehension to merge two dictionaries where the keys will be the movie title and the values will be a list of number of retweets, rating, and box office performance, and save this data in a dictionary titled movie_dict_stats
 
- 
+# Use a query to find all the lead actors in the movies you are looking at. Then use set comprehension in order to store those actors without the possibility of one repeating in a varaible named movie_actors 
+statement = 'SELECT top_actor FROM Movies'
+result = cur.execute(statement)
+movie_actors = {r[0] for r in result.fetchall()}
 
-#List comprehension 
+file_summary.write("\nFrom these movies, the main actors were:\n")
+for actor in movie_actors:
+	file_summary.write(actor)
+	file_summary.write("\n")
+print (movie_actors)
 
 
+file_summary.write("\n\nFrom all this data, the movie I would recommend you see is: " + top_movie_retweets[0] + "\n")
+if top_movie_retweets[0] == "Beauty and the Beast":
+	file_summary.write(movie_insts[0].__str__())
+elif top_movie_retweets[0] == "The Boss Baby":
+	file_summary.write(movie_insts[1].__str__())
+elif top_movie_retweets[0] == "Logan":
+	file_summary.write(movie_insts[2].__str__())
 
-
-
+file_summary.close()
 # Write file named final_project_summary that will have all your findings in it. 
 
-#file_summary = open("final_project_summary.txt", 'w')
 
 
+#Write more test cases
 
 
 
@@ -309,34 +376,52 @@ print ("\n\n BELOW THIS LINE IS OUTPUT FROM TESTS:\n")
 
 class Task1(unittest.TestCase):
 	def test_tweet_caching(self):
-		f = open("SI206_finalproject_cache.json", "r").read()
-		self.assertTrue("tweets" in f)
+		name ="SI206_finalproject_cache.json"
+		f = open(name, 'r')
+		self.assertTrue("Beauty and the Beast" in f.read())
+		f.close()
 	def test_movie_caching(self):
-		f = open("SI206_finalproject_cache.json", "r").read()
-		self.assertTrue("movies" in f)
+		name ="SI206_finalproject_cache.json"
+		f = open(name, 'r')
+		self.assertTrue("OMDb_Logan" in f.read())
+		f.close()
 	def test_get_user_tweets(self):
-		res = get_user_tweets("will smith")
+		res = get_twitter_data("#Logan")
 		self.assertEqual(type(res), type(['hi', 5]))
 	def test_movie_string(self):
-		i = Movie(movie_dict)
-		self.assertEqual(type(i.__str__(), str))
+		mov = get_movie_data("Logan")
+		i = Movie(mov)
+		self.assertEqual(type(i.__str__()), str)
 	def test_movie_title(self):
-		i = Movie(movie_dict)
-		self.assertEqual(type(i.title), type("Inception"))
+		mov = get_movie_data("Logan")
+		i = Movie(mov)
+		self.assertEqual(i.title,"Logan")
 	def test_get_actors(self):
-		i = Movie(movie_dict)
-		self.assertEqual(type(i.get_actors()), type(['hello']))
-	def test_actors_tweets(self):
-		i = Movie(movie_dict)
-		actor = i.get_actors()[0]
-		actor_data = get_user_tweets(actor)
-		self.assertEqual(actor_data[1]['user']['screen_name'], actor)
+		mov = get_movie_data("Logan")
+		i = Movie(mov)
+		self.assertEqual(type(i.get_actors()), type('hello'))
+	def test_movies3(self):
+		self.assertEqual(len(movies), 3)
 	def test_get_user2(self):
-		l = get_user_tweets("will smith")
+		l = get_twitter_data("#beautyandthebeast")
 		self.assertEqual(type(l[1]), type({}))
 	def test_str(self):
-		i = Movie(movie_dict)
+		mov = get_movie_data("Logan")
+		i = Movie(mov)
 		self.assertIn(str(i.title), i.__str__())
+	def test_tweet_table_method(self):
+		self.assertIn(twitter_insts[0].text, twitter_insts[0].get_twitter_table())
+	def test_tweet_class(self):
+		self.assertIn(twitter_insts[0].associated_movie, ['Beauty and the Beast', "Logan", 'The Boss Baby'])
+	def test_user_table_method(self):
+		self.assertIn(twitter_insts[0].userid, twitter_insts[0].get_users_table())
+	def test_get_movie_table_method(self):
+		mov = get_movie_data("Beauty and the Beast")
+		i = Movie(mov)
+		self.assertIn("Bill Condon", i.get_movie_table())
+
+
+
 
 
 
